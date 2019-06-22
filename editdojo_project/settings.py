b1b1b5461e4e148886500829f08a526484d9adf8
umapradeepan <35634716+umapradeepan@users.noreply.github.com>
2019-06-22 17:53:51 -0500

    Update settings.py

--- editdojo_project/settings.py
+++ editdojo_project/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'ls*kn2vqwoomq*rjm!!o)xy&*w^3dwf6w*9*oa$#n1%pxx$2t_'
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'ls*kn2vqwoomq*rjm!!o)xy&*w^3dwf6w*9*oa$#n1%pxx$2t_'
+# SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
